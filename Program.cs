6f3d8292627c87652bccfbc462ebc0b41026b476
Christopher Francisco <cjfg00gl3@gmail.com>
2024-10-12 11:53:25 -0500

    UPDATE Program.cs: load the configuration settings to provide access to the necessary tokens to complete the get request

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Loading configuration settings
+builder.Configuration.AddJsonFile("secrets.json", optional: false, reloadOnChange: true);
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,6 +24,7 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+// Whats the qucking weather
 app.MapGet("/weatherforecast", () =>
 {
     var forecast =  Enumerable.Range(1, 5).Select(index =>
