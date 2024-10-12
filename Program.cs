0b9aba8c6e6e25488d6bf578c031d68a394350b1
Christopher Francisco <cjfg00gl3@gmail.com>
2024-10-12 12:00:16 -0500

    UPDATE Program.cs to create http get endpoint to verify if a user exists provided their username

--- Program.cs
+++ Program.cs
@@ -40,6 +40,43 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+// Verify user provided a username
+app.MapGet("/verifyuser", async (string username, IConfiguration configuration) => {
+
+    // Setup http client
+    using var httpClient = new HttpClient();
+    // Save the bearer token
+    string bearerToken = configuration["BearerToken"];
+
+    // Add the authorization header
+    httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", bearerToken);
+
+    // Build the url
+    string url = $"https://api.twitter.com/2/users/by/username/{username}";
+
+    // Check
+    //Console.WriteLine(bearerToken);
+    //Console.WriteLine(url);
+
+    // Make request
+    HttpResponseMessage response = await httpClient.GetAsync(url);
+    try
+    {
+        // Ensure success
+        response.EnsureSuccessStatusCode();
+
+        // Return response
+        string responseBody = await response.Content.ReadAsStringAsync();
+        return responseBody;
+    }
+    catch (HttpRequestException e)
+    {
+        return await response.Content.ReadAsStringAsync();
+    }
+})
+.WithName("VerifyUser")
+.WithOpenApi();
+
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
